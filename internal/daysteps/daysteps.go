d88772eabed92518974f452413e660314e62b385
deerholt <deerholt.specific@gmail.com>
2025-09-17 17:20:31 +0300

    first-initiation

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,13 +1,64 @@
+// Пакет trainings формирует сводку после дневной прогулки
 package daysteps
 
+import (
+	"errors"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/personaldata"
+	"github.com/Yandex-Practicum/tracker/internal/spentenergy"
+)
+
 type DaySteps struct {
-	// TODO: добавить поля
+	Steps    int
+	Duration time.Duration
+	personaldata.Personal
 }
 
+// Parse заполняет данные о прошедшей тренировке.
+// При недействительных значениях выводит ошибку.
 func (ds *DaySteps) Parse(datastring string) (err error) {
-	// TODO: реализовать функцию
+	data := strings.Split(datastring, ",")
+	if len(data) != 2 {
+		return errors.New("data is invalid")
+	}
+
+	ds.Steps, err = strconv.Atoi(data[0])
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	if ds.Steps <= 0 {
+		return errors.New("steps must be greater than 0")
+	}
+
+	ds.Duration, err = time.ParseDuration(data[1])
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	if ds.Duration <= 0 {
+		return errors.New("duration is not positive")
+	}
+
+	return nil
 }
 
+// ActionInfo выодит результаты о прошедшей тренировку на основе полученных данных.
 func (ds DaySteps) ActionInfo() (string, error) {
-	// TODO: реализовать функцию
+	distance := spentenergy.Distance(ds.Steps, ds.Height)
+
+	calories, err := spentenergy.WalkingSpentCalories(ds.Steps, ds.Weight, ds.Height, ds.Duration)
+	if err != nil {
+		log.Println(err)
+		return "", err
+	}
+
+	return fmt.Sprintf(
+		"Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n",
+		ds.Steps, distance, calories), nil
 }
--- internal/trainings/trainings.go
+++ internal/trainings/trainings.go
@@ -1,13 +1,83 @@
+// Пакет trainings формирует сводку после тренировки.
 package trainings
 
+import (
+	"errors"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/personaldata"
+	"github.com/Yandex-Practicum/tracker/internal/spentenergy"
+)
+
 type Training struct {
-	// TODO: добавить поля
+	Steps        int
+	TrainingType string
+	Duration     time.Duration
+	personaldata.Personal
 }
 
+// Parse заполняет данные о прошедшей тренировке.
+// При недействительных значениях выводит ошибку.
 func (t *Training) Parse(datastring string) (err error) {
-	// TODO: реализовать функцию
+	data := strings.Split(datastring, ",")
+	if len(data) != 3 {
+		return errors.New("data is invalid")
+	}
+
+	t.Steps, err = strconv.Atoi(data[0])
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	if t.Steps <= 0 {
+		return errors.New("steps must be greater than 0")
+	}
+
+	t.TrainingType = data[1]
+	t.Duration, err = time.ParseDuration(data[2])
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	if t.Duration <= 0 {
+		return errors.New("duration is not positive")
+	}
+
+	return nil
 }
 
+// ActionInfo выодит результаты о прошедшей тренировку на основе полученных данных.
 func (t Training) ActionInfo() (string, error) {
-	// TODO: реализовать функцию
+	var (
+		calories float64
+		err      error
+	)
+	distance := spentenergy.Distance(t.Steps, t.Height)
+	averageSpeed := spentenergy.MeanSpeed(t.Steps, t.Height, t.Duration)
+
+	switch t.TrainingType {
+	case "Ходьба":
+		calories, err = spentenergy.WalkingSpentCalories(t.Steps, t.Weight, t.Height, t.Duration)
+		if err != nil {
+			log.Println(err)
+			return "", err
+		}
+	case "Бег":
+		calories, err = spentenergy.RunningSpentCalories(t.Steps, t.Weight, t.Height, t.Duration)
+		if err != nil {
+			log.Println(err)
+			return "", err
+		}
+	default:
+		return "", errors.New("неизвестный тип тренировки")
+	}
+
+	result := fmt.Sprintf(
+		"Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n",
+		t.TrainingType, t.Duration.Hours(), distance, averageSpeed, calories)
+	return result, nil
 }
